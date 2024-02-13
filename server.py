4410d5eeb35711c61f6c69be1aa0e07a103d2e5f
ackermanjayjay <reza23198@gmail.com>
2024-02-13 08:03:28 +0700

    fix

--- server.py
+++ server.py
@@ -7,21 +7,17 @@ app = Flask(__name__)
 
 CORS(app)
 
-
-
-
 # Get all tasks
-@app.route('/books', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def get_tasks():
     if summarizer is not None:
         return jsonify({'tasks': summarizer})
 
-@app.route('/api/books', methods=['GET'])
+@app.route('/api/prediction', methods=['GET'])
 def search_books():
     search_query = request.args.get('q')  # Get search query from query parameters
     result = []
     if search_query:
-        # Filter books whose title contains the search query
         filtered_books = summarizer(search_query, max_length = 150, min_length = 10, do_sample=False)
         result = filtered_books[0]
         
--- src/services/GetApi.js
+++ src/services/GetApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export async function GetApi (){
-    const response =  await axios.get("http://127.0.0.1:5000/books")
+    const response =  await axios.get("http://127.0.0.1:5000/api")
     return response.data.tasks
 }
 
 export async function SearchApi (searchQuery){
-    const search =  await axios.get(`http://127.0.0.1:5000/api/books?q=${searchQuery}`)
+    const search =  await axios.get(`http://127.0.0.1:5000/api/prediction?q=${searchQuery}`)
     return search.data
 }
